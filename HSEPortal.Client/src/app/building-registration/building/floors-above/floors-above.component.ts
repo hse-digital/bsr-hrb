87683e481aac74a88c2006c76b81595f3f8a786a
Diego Santin <Diego Santin>
2023-01-26 13:57:37 -0300

    adding floors-above screen

--- HSEPortal.Client/src/app/building-registration/building/floors-above/floors-above.component.ts
+++ HSEPortal.Client/src/app/building-registration/building/floors-above/floors-above.component.ts
@@ -3,4 +3,18 @@ import { Component } from "@angular/core";
 @Component({
     templateUrl: './floors-above.component.html'
 })
-export class FloorsAboveComponent {}
+export class FloorsAboveComponent {
+
+    building: { floorsAbove?: number } = {};
+    showError = false;
+
+    updateErrorStatus() {
+        this.showError = !this.building.floorsAbove;
+    }
+
+    getErrorText() {
+        return this.showError && !this.building.floorsAbove
+            ? 'Enter the number of floors above ground level for this block'
+            : undefined;
+    }
+}
