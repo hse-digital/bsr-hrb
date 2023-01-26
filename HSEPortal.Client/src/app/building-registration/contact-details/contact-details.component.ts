2a8185fd81325ebd564822d0024b3159ae155f10
Diego Santin <Diego Santin>
2023-01-26 09:35:23 -0300

    updating model binding on contact-details

--- HSEPortal.Client/src/app/building-registration/contact-details/contact-details.component.ts
+++ HSEPortal.Client/src/app/building-registration/contact-details/contact-details.component.ts
@@ -2,25 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hse-contact-details',
-  templateUrl: './contact-details.component.html',
-  styleUrls: ['./contact-details.component.scss']
+  templateUrl: './contact-details.component.html'
 })
 export class ContactDetailsComponent {
 
+  contactDetails: { firstName?: string, lastName?: string } = {};
   showError: boolean = false;
 
-  hasFirstName: boolean = false;
-  hasLastName: boolean = false;
-
   updateErrorStatus() {
-    this.showError = !this.hasFirstName || !this.hasLastName;
+    this.showError = !this.contactDetails.firstName || !this.contactDetails.lastName;
   }
 
-  firstNameModelChange(event: any) {
-    this.hasFirstName = event.length > 0;
+  getContinueLink(): string | undefined {
+    return !this.showError && this.contactDetails.firstName && this.contactDetails.lastName
+      ? '/building-registration'
+      : undefined;
   }
 
-  lastNameModelChange(event: any) {
-    this.hasLastName = event.length > 0;
+  getErrorText(value: any, errorText: string): string | undefined {
+    return this.showError && !value ? errorText : undefined;
   }
 }
