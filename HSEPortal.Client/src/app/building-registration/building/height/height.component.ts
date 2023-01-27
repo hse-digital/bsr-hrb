047723fc49389d92d7b2482c0942b8b107c7e548
Diego Santin <Diego Santin>
2023-01-27 09:40:07 -0300

    fixing validation login in building height

--- HSEPortal.Client/src/app/building-registration/building/height/height.component.ts
+++ HSEPortal.Client/src/app/building-registration/building/height/height.component.ts
@@ -1,26 +1,39 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { BaseFormComponent } from "src/app/helpers/base-form.component";
 
 @Component({
     templateUrl: './height.component.html'
 })
-export class BuildingHeightComponent {
+export class BuildingHeightComponent extends BaseFormComponent {
+    // 'Enter the block height in metres'
 
+    constructor(router: Router) {
+        super(router);
+    }
+
+    nextScreenRoute: string = '/building-registration/building/residential-units';
     building: { height?: number } = {};
-    showError = false;
+    heightHasErrors = false;
 
-    updateErrorStatus() {
-        this.showError = !this.building.height;
-    }
+    errorSummaryMessage: string = 'You must enter the height of this block from ground level to the top floor in metres';
+    errorMessage: string = 'Enter the block height in metres';
 
-    getErrorText() {
-        return this.showError && !this.building.height
-            ? 'Enter the block height in metres'
-            : undefined;
-    }
+    canContinue(): boolean {
+        this.heightHasErrors = true;
 
-    getContinueLink() {
-        return !this.showError && this.building.height
-          ? '/building-registration/building/residential-units'
-          : undefined;
+        if (!this.building.height) {
+            this.errorMessage = 'Enter the block height in metres';
+            this.errorSummaryMessage = 'You must enter the height of this block from ground level to the top floor in metres';
+        } else if (this.building.height >= 1000) {
+            this.errorSummaryMessage = this.errorMessage = 'Block height in metres must be 999.9 or less';
+        } else if (this.building.height < 3) {
+            this.errorSummaryMessage = this.errorMessage = 'Block height in metres must be more than 2';
+        } else {
+            this.heightHasErrors = false;
+            this.errorMessage = '';
+        }
+
+        return !this.heightHasErrors;
     }
 }
