d1a29b59b15b74cb037b0554694840bfeb4844f4
Diego Santin <Diego Santin>
2023-01-27 11:29:43 -0300

    udpating package for hint fixes

--- HSEPortal.Client/src/app/building-registration/building/people-living-in-building/people-living-in-building.component.ts
+++ HSEPortal.Client/src/app/building-registration/building/people-living-in-building/people-living-in-building.component.ts
@@ -1,26 +1,22 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { BaseFormComponent } from "src/app/helpers/base-form.component";
 
 @Component({
     templateUrl: './people-living-in-building.component.html'
 })
-export class PeopleLivingInBuildingComponent {
+export class PeopleLivingInBuildingComponent extends BaseFormComponent {
 
+    constructor(router: Router) {
+        super(router);
+    }
+
+    nextScreenRoute: string = '/building-registration/building/people-living-in-building';
     building: { peopleLivingInBuilding?: any } = {};
-    showError = false;
+    peopleLivingHasErrors = false;
 
-    updateErrorStatus() {
-        this.showError = !this.building.peopleLivingInBuilding;
-    }
-
-    getErrorText() {
-        return this.showError && !this.building.peopleLivingInBuilding
-            ? 'Select if people are living in the block or not'
-            : undefined;
-    }
-
-    getContinueLink() {
-        return !this.showError && this.building.peopleLivingInBuilding
-          ? '/building-registration/building/people-living-in-building'
-          : undefined;
+    canContinue(): boolean {
+        this.peopleLivingHasErrors = !this.building.peopleLivingInBuilding;
+        return !this.peopleLivingHasErrors;
     }
 }
