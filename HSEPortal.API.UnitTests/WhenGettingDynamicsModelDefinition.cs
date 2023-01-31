e89a2b605eb025cc3d76e8cfc03b21d93668902c
OBellochDiaz <odiaz@codec.ie>
2023-01-31 17:38:54 +0000

    Implementation of the Unit Tests

--- HSEPortal.API.UnitTests/WhenGettingDynamicsModelDefinition.cs
+++ HSEPortal.API.UnitTests/WhenGettingDynamicsModelDefinition.cs
@@ -1,6 +1,7 @@
 using HSEPortal.API.Dynamics;
 using HSEPortal.API.Models;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace HSEPortal.API.UnitTests;
 
@@ -9,6 +10,8 @@ public abstract class WhenGettingDynamicsModelDefinition<T> where T : DynamicsEn
 {
     private DynamicsModelDefinitionFactory dynamicsModelDefinitionFactory = null!;
     protected abstract string Endpoint { get; }
+    protected abstract T DynamicsEntity { get; }
+    protected abstract object DynamicsModel { get; }
 
     [SetUp]
     public void Setup()
@@ -33,15 +36,51 @@ public abstract class WhenGettingDynamicsModelDefinition<T> where T : DynamicsEn
     [Test]
     public void ShouldMapModelToDynamicsModel()
     {
+        var modelDefinition = dynamicsModelDefinitionFactory.GetDefinitionFor<T>();
+        var dynamicsModel = modelDefinition.BuildDynamicsModel(DynamicsEntity);
+
+        Assert.IsTrue(NUnitComparer.Equals(dynamicsModel, DynamicsModel));
     }
 }
 
 public class WhenGettingContactDetailsModelDefinition : WhenGettingDynamicsModelDefinition<ContactDetails>
 {
     protected override string Endpoint => "contacts";
+    protected override ContactDetails DynamicsEntity { get; } = new ContactDetails
+    {
+        Id = "1",
+        FirstName = "abc",
+        LastName = "def",
+        Email = "email@email.com",
+        PhoneNumber = "+441234567896"
+    };
+    protected override object DynamicsModel { get; } = new
+    {
+        contactid = "1",
+        firstname = "abc",
+        lastname = "def",
+        emailaddress1 = "email@email.com",
+        telephone1 = "+441234567896"
+    };
 }
 
 public class WhenGettingBuildingDetailsModelDefinition : WhenGettingDynamicsModelDefinition<BuildingDetails>
 {
     protected override string Endpoint => "bsr_blocks";
+    protected override BuildingDetails DynamicsEntity { get; } = new BuildingDetails()
+    {
+        Id = "1",
+        FloorsAbove = 300,
+        Height = 800.5,
+        PeopleLivingInBlock = PeopleLivingInBuilding.Yes,
+        ResidentialUnits = 500
+    };
+    protected override object DynamicsModel { get; } = new
+    {
+        bsr_blockid = "1",
+        bsr_numberofresidentialunits = 500,
+        bsr_arepeoplelivingintheblock = PeopleLivingInBuilding.Yes,
+        bsr_blockheightinmetres = 800.5,
+        bsr_nooffloorsabovegroundlevel = 300,
+    };
 }
