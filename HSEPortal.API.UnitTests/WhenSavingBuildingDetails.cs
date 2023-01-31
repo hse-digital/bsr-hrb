056f204438ec61273bcecdc74f7ab1bfdd26857a
OBellochDiaz <odiaz@codec.ie>
2023-01-31 12:44:39 +0000

    Implementing the API Functions and the UnitTests.

---
+++ HSEPortal.API.UnitTests/WhenSavingBuildingDetails.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Flurl.Http.Testing;
+using NUnit.Framework;
+
+namespace HSEPortal.API.UnitTests
+{
+    internal class WhenSavingBuildingDetails
+    {
+        private HttpTest httpTest;
+        private BuildingFunctions buildingFunctions;
+
+        [SetUp]
+        public void Setup()
+        {
+            httpTest = new HttpTest();
+            buildingFunctions = new BuildingFunctions();
+        }
+
+        [Test]
+        [Category("BuildingDetailsName")]
+        public async Task ShouldCallDynamicsWithBuildingDetailsName()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Add("buildingName", "Building Name");
+
+            IFlurlResponse response = await buildingFunctions.SaveBuildingName(request);
+
+            httpTest.ShouldHaveCalled("https://dynamicsapi")
+                    .WithRequestJson(new { buildingName = "Building Name" });
+
+            Assert.AreEqual(200, response.StatusCode);
+        }
+
+        [Test]
+        [Category("BuildingDetailsFloorsAbove")]
+        public async Task ShouldCallDynamicsWithBuildingDetailsFloorsAbove()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Add("floorsAbove", "35");
+
+            IFlurlResponse response = await buildingFunctions.SaveBuildingFloorsAbove(request);
+
+            httpTest.ShouldHaveCalled("https://dynamicsapi")
+                    .WithRequestJson(new { floorsAbove = "35" });
+
+            Assert.AreEqual(200, response.StatusCode);
+        }
+
+        [Test]
+        [Category("BuildingDetailsHeight")]
+        public async Task ShouldCallDynamicsWithBuildingDetailsHeight()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Add("height", "500");
+
+            IFlurlResponse response = await buildingFunctions.SaveBuildingHeight(request);
+
+            httpTest.ShouldHaveCalled("https://dynamicsapi")
+                    .WithRequestJson(new { height = "500" });
+
+            Assert.AreEqual(200, response.StatusCode);
+        }
+
+        [Test]
+        [Category("BuildingDetailsPeopleLivingInBuilding")]
+        [TestCase("760810000")]
+        [TestCase("760810001")]
+        [TestCase("760810002")]
+        public async Task ShouldCallDynamicsWithBuildingDetailsPeopleLivingInBuilding(string radioValue)
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Add("peopleLivingInBuilding", radioValue);
+
+            IFlurlResponse response = await buildingFunctions.SaveBuildingPeopleLivingInBuilding(request);
+
+            httpTest.ShouldHaveCalled("https://dynamicsapi")
+                    .WithRequestJson(new { peopleLivingInBuilding = radioValue });
+
+            Assert.AreEqual(200, response.StatusCode);
+        }
+
+        [Test]
+        [Category("BuildingDetailsResidentialUnits")]
+        public async Task ShouldCallDynamicsWithBuildingDetailsResidentialUnits()
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Headers.Add("residentialUnits", "250");
+
+            IFlurlResponse response = await buildingFunctions.SaveBuildingResidentialUnits(request);
+
+            httpTest.ShouldHaveCalled("https://dynamicsapi")
+                    .WithRequestJson(new { residentialUnits = "250" });
+
+            Assert.AreEqual(200, response.StatusCode);
+        }
+    }
+}
--- HSEPortal.API.UnitTests/WhenSavingContactDetails.cs
+++ HSEPortal.API.UnitTests/WhenSavingContactDetails.cs
@@ -1,6 +1,9 @@
+using Azure;
+using Flurl.Http;
 using Flurl.Http.Testing;
 using Microsoft.Azure.Functions.Worker.Http;
 using NUnit.Framework;
+using System.Net;
 
 namespace HSEPortal.API.UnitTests;
 
@@ -18,10 +21,48 @@ public class WhenSavingContactDetails
     }
 
     [Test]
-    public async Task ShouldCallDynamicsWithContactData()
+    [Category("ContactDetailsName")]
+    public async Task ShouldCallDynamicsWithContactDetailsName()
     {
-        await contactFunctions.SaveContactDetails(new HttpRequestMessage());
+        HttpRequestMessage request = new HttpRequestMessage();
+        request.Headers.Add("firstName", "First Name");
+        request.Headers.Add("lastName", "Last Name");
 
-        httpTest.ShouldHaveCalled("https://dynamicsapi");
+        IFlurlResponse response = await contactFunctions.SaveContactDetailsName(request);
+
+        httpTest.ShouldHaveCalled("https://dynamicsapi")
+                .WithRequestJson(new { firstName = "First Name", lastName = "Last Name" });
+
+        Assert.AreEqual(200, response.StatusCode);
+    }
+
+    [Test]
+    [Category("ContactDetailsPhoneNumber")]
+    public async Task ShouldCallDynamicsWithContactDetailsPhoneNumber()
+    {
+        HttpRequestMessage request = new HttpRequestMessage();
+        request.Headers.Add("phoneNumber", "+441234567890");
+
+        IFlurlResponse response = await contactFunctions.SaveContactDetailsPhoneNumber(request);
+
+        httpTest.ShouldHaveCalled("https://dynamicsapi")
+                .WithRequestJson(new { phoneNumber = "+441234567890" });
+
+        Assert.AreEqual(200, response.StatusCode);
+    }
+
+    [Test]
+    [Category("ContactDetailsEmail")]
+    public async Task ShouldCallDynamicsWithContactDetailsEmail()
+    {
+        HttpRequestMessage request = new HttpRequestMessage();
+        request.Headers.Add("email", "email@email.com");
+
+        IFlurlResponse response = await contactFunctions.SaveContactDetailsEmail(request);
+
+        httpTest.ShouldHaveCalled("https://dynamicsapi")
+                .WithRequestJson(new { email = "email@email.com" });
+
+        Assert.AreEqual(200, response.StatusCode);
     }
 }
