8743ac76c90c7544c2c20511e7100fb4fbb1c963
OBellochDiaz <odiaz@codec.ie>
2023-02-01 17:21:30 +0000

    Creation of the integration tests projects.

---
+++ HSEPortal.API.IntegrationTests/UnitTestBase.cs
@@ -0,0 +1,71 @@
+using System.Text.Json;
+using Flurl.Http;
+using Flurl.Http.Testing;
+using HSEPortal.Domain.DynamicsDefinitions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+
+namespace HSEPortal.API.IntegrationTests;
+
+[TestFixture]
+public abstract class UnitTestBase
+{
+    protected readonly DynamicsOptions DynamicsOptions = new()
+    {
+        EnvironmentUrl = "http://dynamics.api",
+        TenantId = "1AEA2273-3130-4432-ABB5-9E45BED87E26",
+        ClientId = "77C07F1C-2FB1-4C9F-9C99-82C468AF8299",
+        ClientSecret = "BA8787F6-C52B-49F8-B1D1-F9E54754EEF7"
+    };
+
+    protected HttpTest HttpTest { get; private set; } = null!;
+    protected DynamicsService DynamicsService { get; private set; } = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        FlurlHttp.Configure(settings => { settings.JsonSerializer = new SystemTextJsonSerializer(); });
+
+        var options = new Mock<IOptions<DynamicsOptions>>();
+        options.SetupGet(x => x.Value).Returns(DynamicsOptions);
+
+        HttpTest = new HttpTest();
+        DynamicsService = new DynamicsService(new DynamicsModelDefinitionFactory(), options.Object);
+        AdditionalSetup();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Remove the new elements.
+        HttpTest?.Dispose();
+    }
+
+    protected virtual void AdditionalSetup()
+    {
+    }
+
+    protected HttpRequestData BuildHttpRequestData<T>(T data)
+    {
+        var functionContext = new Mock<FunctionContext>();
+        var requestData = new Mock<HttpRequestData>(functionContext.Object);
+
+        var memoryStream = new MemoryStream();
+        JsonSerializer.Serialize(memoryStream, data);
+
+        memoryStream.Flush();
+        memoryStream.Seek(0, SeekOrigin.Begin);
+
+        requestData.SetupGet(x => x.Body).Returns(memoryStream);
+
+        return requestData.Object;
+    }
+
+    protected object BuildODataEntityHeader(string id)
+    {
+        return $"OData-EntityId={DynamicsOptions.EnvironmentUrl}/api/data/v9.2/whatever_entity({id})";
+    }
+}
---
+++ HSEPortal.API.IntegrationTests/WhenCreatingABuildingApplication.cs
@@ -0,0 +1,70 @@
+﻿using Flurl.Http;
+using Flurl.Http.Testing;
+using Grpc.Core;
+using HSEPortal.API.Functions;
+using HSEPortal.API.Model;
+using HSEPortal.Domain.Entities;
+using NUnit.Framework;
+
+namespace HSEPortal.API.IntegrationTests;
+
+public class WhenCreatingABuildingApplication : UnitTestBase
+{
+    private BuildingApplicationFunctions buildingApplicationFunctions = null!;
+    private const string dynamicsAuthToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkd";
+    private const string buildingApplicationReturnId = "EC6B32C8-0188-4CCE-B58C-D6F05FEEF79B";
+    private const string buildingReturnId = "06F8C7E4-F41A-4EB4-B8E2-3501701A4A53";
+
+    protected override void AdditionalSetup()
+    {
+        buildingApplicationFunctions = new BuildingApplicationFunctions(DynamicsService);
+        HttpTest.RespondWithJson(new DynamicsAuthenticationModel { AccessToken = dynamicsAuthToken });
+        HttpTest.RespondWith(status: 204, headers: BuildODataEntityHeader(buildingApplicationReturnId));
+        HttpTest.RespondWith(status: 204, headers: BuildODataEntityHeader(buildingReturnId));
+    }
+
+    [Test]
+    public async Task ShouldAcquireAuthenticationTokenForDynamics()
+    {
+        var buildingApplicationModel = GivenABuildingApplicationModel();
+        await WhenSendingANewBuildingApplication(buildingApplicationModel);
+
+        HttpTest.ShouldHaveCalled($"https://login.microsoftonline.com/{DynamicsOptions.TenantId}/oauth2/token")
+            .WithRequestUrlEncoded(new
+            {
+                grant_type = "client_credentials",
+                client_id = DynamicsOptions.ClientId,
+                client_secret = DynamicsOptions.ClientSecret,
+                resource = DynamicsOptions.EnvironmentUrl
+            });
+    }
+
+    [Test]
+    public async Task ShouldCreateRelatedEntities()
+    {
+        var buildingApplicationModel = GivenABuildingApplicationModel();
+        await WhenSendingANewBuildingApplication(buildingApplicationModel);
+
+        // HttpTest
+        
+    }
+
+    private BuildingApplicationModel GivenABuildingApplicationModel()
+    {
+        return new BuildingApplicationModel
+        {
+            BuildingName = "BuildingName",
+            ContactFirstName = "Diego",
+            ContactLastName = "Santin",
+            ContactPhoneNumber = "+44 808 157 0192",
+            ContactEmailAddress = "dsantin@codec.ie"
+        };
+    }
+
+    private async Task WhenSendingANewBuildingApplication(BuildingApplicationModel buildingApplicationModel)
+    {
+        var requestData = BuildHttpRequestData(buildingApplicationModel);
+        await buildingApplicationFunctions.NewBuildingApplication(requestData);
+    }
+}
+
