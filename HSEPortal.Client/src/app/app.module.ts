9bbac500be713086c325fdeafb24c752d558995a
Diego Santin <Diego Santin>
2023-02-09 12:27:29 -0300

    moving idle timer init to component

--- HSEPortal.Client/src/app/app.module.ts
+++ HSEPortal.Client/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { Router } from '@angular/router';
 import { HseAngularModule } from 'hse-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './features/landing/landing.component';
 import { TimeoutComponent } from './features/timeout/timeout.component';
-import { IdleTimerService } from './services/idle-timer.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +24,4 @@ import { IdleTimerService } from './services/idle-timer.service';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(private router: Router, private idleTimerService: IdleTimerService) {
-    this.idleTimerService.initTimer(15 * 60, () => { this.router.navigate(['/building-registration/continue-saved-application']) });
-  }
 }
