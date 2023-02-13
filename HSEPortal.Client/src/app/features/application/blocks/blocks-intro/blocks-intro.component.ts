e6e763e3329cf789df825ff11503804c07fbea3a
Diego Santin <Diego Santin>
2023-02-13 07:12:49 -0300

    making blocks intro base component

--- HSEPortal.Client/src/app/features/application/blocks/blocks-intro/blocks-intro.component.ts
+++ HSEPortal.Client/src/app/features/application/blocks/blocks-intro/blocks-intro.component.ts
@@ -1,15 +1,26 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { BaseComponent } from "src/app/helpers/base.component";
+import { IHasNextPage } from "src/app/helpers/has-next-page.interface";
 import { ApplicationService } from "src/app/services/application.service";
 import { NavigationService } from "src/app/services/navigation.service";
 
 @Component({
   templateUrl: './blocks-intro.component.html'
 })
-export class BuildingBlocksIntroComponent {
+export class BuildingBlocksIntroComponent extends BaseComponent implements IHasNextPage {
 
-  constructor(public router: Router, applicationService: ApplicationService, navigationService: NavigationService) {
+  canContinue(): boolean {
+    return true;
+  }
+
+  constructor(router: Router, applicationService: ApplicationService, navigationService: NavigationService, activatedRoute: ActivatedRoute) {
+    super(router, applicationService, navigationService, activatedRoute)
     applicationService.currentBlock = {};
+  }
+
+  navigateToNextPage(navigationService: NavigationService, activatedRoute: ActivatedRoute): Promise<boolean> {
+    return navigationService.navigateRelative('block-name', activatedRoute);
   }
 
 }
