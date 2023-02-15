1faca4f9136c1544440f3c5a2694606fb1e7d5a7
Diego Santin <Diego Santin>
2023-02-15 10:52:44 -0300

    integration test values

--- HSEPortal.API.IntegrationTests/WhenValidatingApplicationNumber.cs
+++ HSEPortal.API.IntegrationTests/WhenValidatingApplicationNumber.cs
@@ -2,7 +2,6 @@ using System.Net;
 using FluentAssertions;
 using Flurl;
 using Flurl.Http;
-using HSEPortal.API.Services;
 using Microsoft.Extensions.Options;
 using Xunit;
 
@@ -10,17 +9,15 @@ namespace HSEPortal.API.IntegrationTests;
 
 public class WhenValidatingApplicationNumber : IntegrationTestBase
 {
-    private readonly IOptions<DynamicsOptions> dynamicsOptions;
     private readonly IOptions<SwaOptions> swaOptions;
 
-    public WhenValidatingApplicationNumber(IOptions<DynamicsOptions> dynamicsOptions, IOptions<SwaOptions> swaOptions)
+    public WhenValidatingApplicationNumber(IOptions<SwaOptions> swaOptions)
     {
-        this.dynamicsOptions = dynamicsOptions;
         this.swaOptions = swaOptions;
     }
 
     [Theory]
-    [InlineData("REGISTRATION_ID", "integration@testing.com", HttpStatusCode.OK)]
+    [InlineData("HBR170597960", "dont@delete.com", HttpStatusCode.OK)]
     [InlineData("DOESNT-EXIST", "DOESNT-EXIST", HttpStatusCode.BadRequest)]
     public async Task ShouldReturnApplicationExistsBasedOnNumberAndEmailAddress(string applicationNumber, string emailAddress, HttpStatusCode expectedResponse)
     {
