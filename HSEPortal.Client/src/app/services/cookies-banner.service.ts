48a0ceda23f411bbb830ff276f83db2d24c243de
OBellochDiaz <odiaz@codec.ie>
2023-03-15 12:13:25 +0000

    Adding Cookies logic.

--- HSEPortal.Client/src/app/services/cookies-banner.service.ts
+++ HSEPortal.Client/src/app/services/cookies-banner.service.ts
@@ -7,13 +7,19 @@ import { LocalStorage } from "src/app/helpers/local-storage";
 export class CookiesBannerService {
 
   cookiesModel!: { showCookies: boolean, cookiesAccepted: boolean };
+  private Cookies: Cookies;
+
+  cookieKey: string = "nonEsentialCookies";
+  cookieExpiresDays = 365;
 
   constructor() {
+    this.Cookies = new Cookies();
     this.initCookiesModel();
   }
 
   getShowCookiesStatus() {
     if (!this.cookiesModel) this.initCookiesModel();
+
     return this.cookiesModel?.showCookies ?? true;
   }
 
@@ -21,21 +27,63 @@ export class CookiesBannerService {
     if (!this.cookiesModel) this.initCookiesModel();
     this.cookiesModel.showCookies = false;
     this.cookiesModel.cookiesAccepted = true;
-    LocalStorage.setJSON("cookies", this.cookiesModel);
+    this.setCookie("true");
   }
 
   rejectCookies() {
     if (!this.cookiesModel) this.initCookiesModel();
     this.cookiesModel.showCookies = false;
     this.cookiesModel.cookiesAccepted = false;
-    LocalStorage.setJSON("cookies", this.cookiesModel);
+    this.setCookie("false");
   }
 
   private initCookiesModel() {
-    this.cookiesModel = LocalStorage.getJSON("cookies");
+    this.cookiesModel = this.parseCookie("nonEsentialCookies");
     if (!this.cookiesModel) {
       this.cookiesModel = { showCookies: true, cookiesAccepted: false };
-      LocalStorage.setJSON("cookies", this.cookiesModel);
     }
+  }
+
+  private setCookie(value: string) {
+    this.Cookies.set(this.cookieKey, value, this.cookieExpiresDays);
+  }
+
+  private parseCookie(cookieKey: string) {
+    let cookie: string = this.Cookies.get(cookieKey);
+    if (cookie) {
+      let value = cookie.substring(cookie.indexOf('=') + 1);
+      this.cookiesModel = {
+        showCookies: value === "true",
+        cookiesAccepted: value === "true"
+      };
+    }
+    return this.cookiesModel;
+  }
+
+  
+}
+
+export class Cookies {
+
+  constructor() { }
+
+  set(key: string, value: string, expiresDays?: number) {
+    let expires = expiresDays ? this.createExpiresValue(expiresDays) : "";
+    this.add(key, value, expires);
+  }
+
+  get(key: string): string {
+    return document.cookie.split(' ').find(x => x.trim().startsWith(`${key}=`)) ?? "";
+  }
+
+  private add(key: string, value: string, expires?: string) {
+    let newCookie = `${key}=${value}${expires ? "; expires=" + expires : ""}`;
+    document.cookie = newCookie;
+  }
+
+  private createExpiresValue(expiresDays: number) {
+    const d = new Date();
+    d.setTime(d.getTime() + (expiresDays * 24 * 60 * 60 * 1000));
+    return `expires=${d.toUTCString()}`;
   }
 }
