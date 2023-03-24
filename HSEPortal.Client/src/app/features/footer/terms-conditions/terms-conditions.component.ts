c3b81336d22d9482a9f3e338251e8300e6c502ed
OBellochDiaz <odiaz@codec.ie>
2023-03-24 16:07:05 +0000

    Adding new links to the footer.

--- HSEPortal.Client/src/app/features/footer/terms-conditions/terms-conditions.component.ts
+++ HSEPortal.Client/src/app/features/footer/terms-conditions/terms-conditions.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './terms-conditions.component.html'
 })
 export class TermsConditionsComponent {
-  public static route: string = "terms-and-conditions";
+  public static route: string = "terms-conditions";
   static title: string = "Terms and conditions - Register a high-rise building - GOV.UK";
 
   constructor() { }
