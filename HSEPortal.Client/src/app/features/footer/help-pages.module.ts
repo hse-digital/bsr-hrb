e99b81c8f117c49ab0059b1f79898db1a25e2cdd
OBellochDiaz <odiaz@codec.ie>
2023-03-24 15:35:46 +0000

    Implementing the accessibility page.

--- HSEPortal.Client/src/app/features/footer/help-pages.module.ts
+++ HSEPortal.Client/src/app/features/footer/help-pages.module.ts
@@ -5,16 +5,19 @@ import { HseRoute, HseRoutes } from 'src/app/services/hse.route';
 import { RouterModule } from '@angular/router';
 import { HseAngularModule } from 'hse-angular';
 import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
+import { AccessibilityComponent } from './accessibility/accessibility.component';
 
 const routes = new HseRoutes([
   HseRoute.unsafe(PrivacyNoticeComponent.route, PrivacyNoticeComponent, undefined, PrivacyNoticeComponent.title),
   HseRoute.unsafe(TermsConditionsComponent.route, TermsConditionsComponent, undefined, TermsConditionsComponent.title),
+  HseRoute.unsafe(AccessibilityComponent.route, AccessibilityComponent, undefined, AccessibilityComponent.title),
 ]);
 
 @NgModule({
   declarations: [
     PrivacyNoticeComponent,
     TermsConditionsComponent,
+    AccessibilityComponent,
   ],
   imports: [
     RouterModule.forChild(routes.getRoutes()),
