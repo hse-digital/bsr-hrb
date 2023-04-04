4a21a697d39d0fdfb3a4ad42f5db83a1306f6be3
OBellochDiaz <odiaz@codec.ie>
2023-04-04 16:36:36 +0100

    Refactoring accountable person check answers page and summary page.

--- HSEPortal.Client/src/app/features/application/accountable-person/check-answers/individual-answers.component.ts
+++ HSEPortal.Client/src/app/features/application/accountable-person/check-answers/individual-answers.component.ts
@@ -1,108 +1,48 @@
 import { Component, Input } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { SummaryComponent } from "src/app/helpers/summary-helper";
 import { AccountablePersonModel } from "src/app/services/application.service";
 import { NavigationService } from "src/app/services/navigation.service";
 
 @Component({
-    selector: 'individual-answers',
-    templateUrl: './individual-answers.component.html'
+  selector: 'individual-answers',
+  templateUrl: './individual-answers.component.html'
 })
-export class IndividualAnswersComponent {
+export class IndividualAnswersComponent extends SummaryComponent {
 
-    constructor(private navigationService: NavigationService, private activatedRoute: ActivatedRoute) { }
+  @Input() override ap!: AccountablePersonModel;
+  @Input() override apIndex!: number;
+  @Input() override hasMoreAp = false;
 
-    @Input() ap!: AccountablePersonModel;
-    @Input() apIndex!: number;
-    @Input() hasMoreAp = false;
+  constructor(private navigationService: NavigationService, private activatedRoute: ActivatedRoute) {
+    super();
+  }
 
-    addMore() {
-        this.navigationService.navigateRelative('add-more', this.activatedRoute);
+  addMore() {
+    this.navigationService.navigateRelative('add-more', this.activatedRoute);
+  }
+
+  goToApType() {
+    if (this.apIndex == 0) {
+      this.navigationService.navigateRelative('', this.activatedRoute);
+    } else {
+      this.navigateTo('accountable-person-type');
     }
+  }
 
-    goToApType() {
-        if (this.apIndex == 0) {
-            this.navigationService.navigateRelative('', this.activatedRoute);
-        } else {
-            this.navigateTo('accountable-person-type');
-        }
-    }
+  navigateTo(url: string, query?: string) {
+    this.navigationService.navigateRelative(`accountable-person-${this.apIndex + 1}/${url}`, this.activatedRoute, {
+      return: 'check-answers'
+    });
+  }
 
-    navigateTo(url: string, query?: string) {
-        this.navigationService.navigateRelative(`accountable-person-${this.apIndex + 1}/${url}`, this.activatedRoute, {
-            return: 'check-answers'
-        });
-    }
+  getYourAddress() {
+    return this.apIndex == 0 && this.ap.IsPrincipal == 'yes' ?
+      this.ap.PapAddress : this.ap.Address;
+  }
 
-    getYourAddress() {
-        return this.apIndex == 0 && this.ap.IsPrincipal == 'yes' ? 
-            this.ap.PapAddress : this.ap.Address;
-    }
-
-    getPapAddress() {
-        return this.apIndex == 0 && this.ap.IsPrincipal == 'no' ? 
-            this.ap.PapAddress : this.ap.Address;
-    }
-
-    notPap() {
-        return this.ap.IsPrincipal == 'no';
-    }
-
-    notLead() {
-        return this.ap.Role == 'registering_for';
-    }
-
-    whoAreYouDescription() {
-        switch (this.ap.Role) {
-            case 'named_contact': return 'I am the named contact';
-            case 'registering_for': return 'I am registering for the named contact';
-        }
-
-        return undefined;
-    }
-
-    useSameAddressDescription() {
-        switch(this.ap.ActingForSameAddress) {
-            case 'yes': return 'Yes, use the same address';
-            case 'no': return 'No, use a different address';
-        }
-
-        return undefined;
-    }
-
-    organisationTypeDescription() {
-        switch (this.ap.OrganisationType) {
-            case 'commonhold-association': return 'Commonhold association';
-            case 'housing-association': return 'Housing association or other company operating under section 27 of the Housing Act 1985';
-            case 'local-authority': return 'Local authority';
-            case 'management-company': return 'Management company';
-            case 'rmc-or-organisation': return 'Resident management company (RMC) or organisation';
-            case 'rtm-or-organisation': return 'Right to manage (RTM) company or organisation';
-        }
-        return this.ap.OrganisationTypeDescription;
-    }
-
-    leadJobRoleDescription() {
-        switch (this.ap.LeadJobRole) {
-            case 'director': return 'Director';
-            case 'administrative_worker': return 'Administrative or office worker';
-            case 'building_manager': return 'Building or facilities manager';
-            case 'building_director': return 'Building safety director';
-        }
-
-        return 'Other';
-    }
-
-    sectionsWithAccountability() {
-        return this.ap.SectionsAccountability?.filter(x => x.Accountability?.length ?? 0 > 0);
-    }
-
-    _accountabilityDescription(value: string) {
-        switch (value) {
-            case 'external_walls': return 'External walls and roof';
-            case 'routes': return 'Routes that residents can walk through';
-            case 'maintenance': return 'Maintaining plant and equipment';
-        }
-
-        return 'Facilities that residents share';
-    }
+  getPapAddress() {
+    return this.apIndex == 0 && this.ap.IsPrincipal == 'no' ?
+      this.ap.PapAddress : this.ap.Address;
+  }
 }
---
+++ HSEPortal.Client/src/app/helpers/summary-helper.ts
@@ -0,0 +1,99 @@
+import { AccountablePersonModel } from "../services/application.service";
+import { FieldValidations } from "./validators/fieldvalidations";
+
+export class SummaryComponent {
+  protected ap!: AccountablePersonModel;
+  protected apIndex!: number;
+  protected hasMoreAp = false;
+
+  get whoAreYouDescription() {
+    return SummaryTextValueMapper.whoAreYouDescription[this.ap.Role ?? ""];
+  }
+
+  get useSameAddressDescription() {
+    return SummaryTextValueMapper.useSameAddressDescription[this.ap.ActingForSameAddress ?? ""]
+  }
+
+  get organisationTypeDescription() {
+    let organisationType = SummaryTextValueMapper.organisationTypeDescription[this.ap.OrganisationType ?? ""];
+
+    return FieldValidations.IsNotNullOrWhitespace(organisationType)
+      ? organisationType
+      : this.ap.OrganisationTypeDescription;
+  }
+
+  get leadJobRoleDescription() {
+    return SummaryTextValueMapper.leadJobRoleDescription[this.ap.LeadJobRole ?? ""];
+  }
+
+  get apAddress() {
+    return this.ap.PapAddress ?? this.ap.Address;
+  }
+
+  sectionsWithAccountability() {
+    return this.ap.SectionsAccountability?.filter(x => x.Accountability?.length ?? 0 > 0);
+  }
+
+  _accountabilityDescription(value: string) {
+    let accountabilityDescription = SummaryTextValueMapper._accountabilityDescription[value];
+    return FieldValidations.IsNotNullOrWhitespace(accountabilityDescription)
+      ? accountabilityDescription
+      : 'Facilities that residents share';
+  }
+
+  notPap() {
+    return this.ap.IsPrincipal == 'no';
+  }
+
+  isRegisteringFor() {
+    return this.ap.Role == 'registering_for';
+  }
+
+  isNamedContact() {
+    return this.ap.Role == 'named_contact';
+  }
+
+  isEmployee() {
+    return this.ap.Role == 'employee';
+  }
+}
+
+type Mapper = Record<string, string>;
+
+class SummaryTextValueMapper {
+
+  static readonly whoAreYouDescription: Mapper = {
+    "named_contact": "I am the named contact",
+    "registering_for": "I am registering for the named contact",
+    "employee": "I am an employee"
+  };
+
+  static readonly useSameAddressDescription: Mapper = {
+    "yes": "Yes, use the same address",
+    "no": "No, use a different address"
+  }
+
+  static readonly organisationTypeDescription: Mapper = {
+    "commonhold-association": "Commonhold association",
+    "housing-association": "Housing association or other company operating under section 27 of the Housing Act 1985",
+    "local-authority": "Local authority",
+    "management-company": "Management company",
+    "rmc-or-organisation": "Resident management company (RMC) or organisation",
+    "rtm-or-organisation": "Right to manage (RTM) company or organisation"
+  }
+
+  static readonly leadJobRoleDescription: Mapper = {
+    "director": "Director",
+    "administrative_worker": "Administrative or office worker",
+    "building_manager": "Building or facilities manager",
+    "building_director": "Building safety director",
+    "other": "Other"
+  }
+
+  static readonly _accountabilityDescription: Mapper = {
+    "external_walls": "External walls and roof",
+    "routes": "Routes that residents can walk through",
+    "maintenance": "Maintaining plant and equipment"
+  }
+
+}
