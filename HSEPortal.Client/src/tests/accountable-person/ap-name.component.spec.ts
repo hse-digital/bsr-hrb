d20def0d66ff4009ba49e5be04f8c3a152e225fc
OBellochDiaz <odiaz@codec.ie>
2023-04-21 09:39:34 +0100

    [WIP] Adding ap unit tests.

--- HSEPortal.Client/src/tests/accountable-person/ap-name.component.spec.ts
+++ HSEPortal.Client/src/tests/accountable-person/ap-name.component.spec.ts
@@ -7,12 +7,13 @@ import { ApplicationService } from 'src/app/services/application.service';
 
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ApDetailsComponent } from 'src/app/features/application/accountable-person/ap-details/ap-details.component';
+
 import { inject } from "@angular/core/testing";
 import { TestHelper } from '../test-helper';
+import { ApNameComponent } from 'src/app/features/application/accountable-person/ap-name/ap-name.component';
 
-let component: ApDetailsComponent;
-let fixture: ComponentFixture<ApDetailsComponent>;
+let component: ApNameComponent;
+let fixture: ComponentFixture<ApNameComponent>;
 
 let httpClient: HttpClient;
 let httpTestingController: HttpTestingController;
@@ -21,10 +22,8 @@ function setup(applicationService: ApplicationService) {
     applicationService.newApplication();
     applicationService.model.AccountablePersons = [];
     applicationService.startAccountablePersonEdit();
-    applicationService.currentAccountablePerson.FirstName = 'FirstName';
-    applicationService.currentAccountablePerson.LastName = "LastName";
 
-    fixture = TestBed.createComponent(ApDetailsComponent);
+    fixture = TestBed.createComponent(ApNameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
 }
@@ -33,7 +32,7 @@ describe('ApDetailsComponent showError', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [ApDetailsComponent],
+            declarations: [ApNameComponent],
             imports: [HttpClientTestingModule, RouterTestingModule, HseAngularModule, ComponentsModule],
             providers: [ApplicationService]
         }).compileComponents();
@@ -48,55 +47,55 @@ describe('ApDetailsComponent showError', () => {
     }));
 
     let testCasesEmptyUndefined = [
-        { email: '', phoneNumber: '' },
-        { email: '', phoneNumber: undefined },
-        { email: undefined, phoneNumber: '' },
-        { email: undefined, phoneNumber: undefined },
+        { firstName: '', lastName: '' },
+        { firstName: '', lastName: undefined },
+        { firstName: undefined, lastName: '' },
+        { firstName: undefined, lastName: undefined },
     ]
 
     new TestHelper()
-        .setDescription("should show an error if the email or the phone number are empty or undefined")
+        .setDescription("should show an error if the first name or the last name are empty or undefined")
         .setTestCase((applicationService: ApplicationService, value: any) => {
             setup(applicationService);
-            applicationService.currentAccountablePerson.Email = value.email;
-            applicationService.currentAccountablePerson.PhoneNumber = value.phoneNumber;
+            applicationService.currentAccountablePerson.FirstName = value.firstName;
+            applicationService.currentAccountablePerson.LastName = value.lastName;
             component.hasErrors = !component.canContinue();
             expect(component.hasErrors).toBeTrue();
         }, ...testCasesEmptyUndefined).execute();
 
     new TestHelper()
-        .setDescription("should show an error if the email is empty or undefined but the phone number is valid")
+        .setDescription("should show an error if the first name is empty or undefined but the last name is valid")
         .setTestCase((applicationService: ApplicationService, value: any) => {
             setup(applicationService);
-            applicationService.currentAccountablePerson.Email = value;
-            applicationService.currentAccountablePerson.PhoneNumber = "+441234567890";
+            applicationService.currentAccountablePerson.FirstName = value;
+            applicationService.currentAccountablePerson.LastName = "Last Name";
             component.hasErrors = !component.canContinue();
             expect(component.hasErrors).toBeTrue();
-            expect(component.emailHasErrors).toBeTrue();
-            expect(component.phoneHasErrors).toBeFalse();
+            expect(component.firstNameInError).toBeFalse();
+            expect(component.lastNameInError).toBeTrue();
         }, '', undefined).execute();
 
     new TestHelper()
-        .setDescription("should show an error if the phone number is empty or undefined but the email is valid")
+        .setDescription("should show an error if the last name is empty or undefined but the first name is valid")
         .setTestCase((applicationService: ApplicationService, value: any) => {
             setup(applicationService);
-            applicationService.currentAccountablePerson.Email = "valid_email@validemail.com";
-            applicationService.currentAccountablePerson.PhoneNumber = value;
+            applicationService.currentAccountablePerson.FirstName = "First Name";
+            applicationService.currentAccountablePerson.LastName = value;
             component.hasErrors = !component.canContinue();
             expect(component.hasErrors).toBeTrue();
-            expect(component.phoneHasErrors).toBeTrue();
-            expect(component.emailHasErrors).toBeFalse();
+            expect(component.firstNameInError).toBeTrue();
+            expect(component.lastNameInError).toBeFalse();
         }, '', undefined).execute();
 
     new TestHelper()
-        .setDescription("should NOT show an error if the phone number and the email are valid")
+        .setDescription("should NOT show an error if the first name and the last name are valid")
         .setTestCase((applicationService: ApplicationService, value: any) => {
             setup(applicationService);
-            applicationService.currentAccountablePerson.Email = value.email;
-            applicationService.currentAccountablePerson.PhoneNumber = value.phoneNumber;
+            applicationService.currentAccountablePerson.FirstName = value.firstName;
+            applicationService.currentAccountablePerson.LastName = value.lastName;
             component.hasErrors = !component.canContinue();
             expect(component.hasErrors).toBeFalse();
-            expect(component.phoneHasErrors).toBeFalse();
-            expect(component.emailHasErrors).toBeFalse();
-        }, {email: "valid_email@validemail.com", phoneNumber: "+441234567890"}).execute();
+            expect(component.firstNameInError).toBeFalse();
+            expect(component.lastNameInError).toBeFalse();
+        }, { lastName: "Last Name", firstName: "First Name"}).execute();
 });
