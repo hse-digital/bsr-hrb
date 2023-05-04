f5ca77b54bdaa16bb3af3e6dcae21d467754f10c
OBellochDiaz <odiaz@codec.ie>
2023-05-04 15:43:24 +0100

    Refactoring areas of accountability screens.

--- HSEPortal.Client/src/app/helpers/accountability-areas-helper.ts
+++ HSEPortal.Client/src/app/helpers/accountability-areas-helper.ts
@@ -2,19 +2,38 @@ import { ApplicationService, SectionAccountability, SectionModel } from "../serv
 
 export class AccountabilityAreasHelper {
 
+    static updateAccountabilityAreas(applicationService: ApplicationService, accountablePersonIndex: number, section: SectionModel, area: string) {
+        let accountability = AccountabilityAreasHelper.getAccountabilityFor(applicationService, accountablePersonIndex, section);
+
+        if (!AccountabilityAreasHelper.isApAccountableFor(applicationService, accountablePersonIndex, section, area)) {
+            accountability.push(area);
+        } else {
+            let areaIndex = accountability.indexOf(area);
+            if (areaIndex) accountability.splice(areaIndex, 1);
+        }
+
+        return accountability;
+    }
+
     static isApAccountableFor(applicationService: ApplicationService, apIndex: number, section: SectionModel, area: string): boolean {
         return applicationService.model.AccountablePersons[apIndex]?.SectionsAccountability
             ?.find(x => x.SectionName == section.Name ?? applicationService.model.BuildingName)
             ?.Accountability?.includes(area) ?? false;
     }
 
-    static getAp(applicationService: ApplicationService, section: SectionModel, area: string) {
-        let aps = []
-        for (let i = 0; i < applicationService.model.AccountablePersons.length; i++) {
-            let sectionAccountability = applicationService.model.AccountablePersons[i].SectionsAccountability?.find(x => x.SectionName == section.Name ?? applicationService.model.BuildingName);
-            if (sectionAccountability?.Accountability?.includes(area)) aps.push(i)
-        }
-        return aps;
+    static getAccountabilityFor(applicationService: ApplicationService, accountablePersonIndex: number, section: SectionModel) {
+        return applicationService.model.AccountablePersons[accountablePersonIndex].SectionsAccountability
+            ?.find(x => x.SectionName == section.Name ?? applicationService.model.BuildingName)?.Accountability!;
+    }
+
+    private static areasOfAccountability: string[] = ["routes", "maintenance", "facilities"];
+    static getNotAllocatedAreasOf(applicationService: ApplicationService, section: SectionModel) {
+      let accountabilityAreasOfSection = applicationService.model.AccountablePersons
+        .flatMap(x => x.SectionsAccountability)
+        .filter(x => x?.SectionName == section.Name ?? applicationService.model.BuildingName!)
+        .flatMap(x => x?.Accountability);
+      let notAllocatedAreas: string[] = this.areasOfAccountability.filter(x => !accountabilityAreasOfSection.includes(x))
+      return notAllocatedAreas;
     }
 
 }
