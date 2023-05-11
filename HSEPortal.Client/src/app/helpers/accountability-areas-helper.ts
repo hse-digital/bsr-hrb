e54161779665a4b176c4c131d7edfe2ff733c7f1
Diego Santin <Diego Santin>
2023-05-11 05:56:46 -0300

    fixing paranthesis on section name check

--- HSEPortal.Client/src/app/helpers/accountability-areas-helper.ts
+++ HSEPortal.Client/src/app/helpers/accountability-areas-helper.ts
@@ -22,20 +22,20 @@ export class AccountabilityAreasHelper {
 
     static isApAccountableFor(applicationService: ApplicationService, apIndex: number, section: SectionModel, area: string): boolean {
         return applicationService.model.AccountablePersons[apIndex]?.SectionsAccountability
-            ?.find(x => x.SectionName == section.Name ?? applicationService.model.BuildingName)
+            ?.find(x => x.SectionName == (section.Name ?? applicationService.model.BuildingName))
             ?.Accountability?.includes(area) ?? false;
     }
 
     static getAccountabilityFor(applicationService: ApplicationService, accountablePersonIndex: number, section: SectionModel) {
         return applicationService.model.AccountablePersons[accountablePersonIndex].SectionsAccountability
-            ?.find(x => x.SectionName == section.Name ?? applicationService.model.BuildingName)?.Accountability ?? [];
+            ?.find(x => x.SectionName == (section.Name ?? applicationService.model.BuildingName))?.Accountability ?? [];
     }
 
     private static areasOfAccountability: string[] = ["routes", "maintenance", "facilities"];
     static getNotAllocatedAreasOf(applicationService: ApplicationService, section: SectionModel) {
         let accountabilityAreasOfSection = applicationService.model.AccountablePersons
             .flatMap(x => x.SectionsAccountability)
-            .filter(x => x?.SectionName == section.Name ?? applicationService.model.BuildingName!)
+            .filter(x => x?.SectionName == (section.Name ?? applicationService.model.BuildingName!))
             .flatMap(x => x?.Accountability);
 
         return this.areasOfAccountability.filter(x => !accountabilityAreasOfSection.includes(x));
