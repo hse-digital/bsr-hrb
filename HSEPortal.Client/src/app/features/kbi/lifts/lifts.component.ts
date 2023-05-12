6cb77b4c0203276c5d3133efeec6d4736186a361
OBellochDiaz <odiaz@codec.ie>
2023-05-12 16:08:25 +0100

    Refatoring canAccess functions.

--- HSEPortal.Client/src/app/features/kbi/lifts/lifts.component.ts
+++ HSEPortal.Client/src/app/features/kbi/lifts/lifts.component.ts
@@ -53,7 +53,7 @@ export class LiftsComponent  extends BaseComponent implements IHasNextPage, OnIn
   }
 
   override canAccess(routeSnapshot: ActivatedRouteSnapshot) {
-    let fireSmokeProvisionIsNone = !!this.applicationService.currenKbiSection!.fireSmokeProvisions && this.applicationService.currenKbiSection!.fireSmokeProvisions!.length == 1 && this.applicationService.currenKbiSection!.fireSmokeProvisions![0] == 'none';
+    let fireSmokeProvisionIsNone = !!this.applicationService.currenKbiSection?.fireSmokeProvisions && this.applicationService.currenKbiSection?.fireSmokeProvisions?.length == 1 && this.applicationService.currenKbiSection!.fireSmokeProvisions![0] == 'none';
     return fireSmokeProvisionIsNone || (!!this.applicationService.currenKbiSection?.fireSmokeProvisionLocations 
               && Object.keys(this.applicationService.currenKbiSection?.fireSmokeProvisionLocations).length > 0
               && this.applicationService.currenKbiSection.fireSmokeProvisions!.every(x => this.applicationService.currenKbiSection!.fireSmokeProvisionLocations![x].length > 0));
