8b5df1692b2760ca5602e40f2c5a7df903bd1b89
OBellochDiaz <odiaz@codec.ie>
2023-06-09 09:44:30 +0100

    Implementing kbi confirm screen.

--- HSEPortal.Client/src/app/features/kbi/9-submit/confirm/confirm.component.ts
+++ HSEPortal.Client/src/app/features/kbi/9-submit/confirm/confirm.component.ts
@@ -4,25 +4,21 @@ import { BaseComponent } from 'src/app/helpers/base.component';
 import { ApplicationService, BuildingApplicationStatus } from 'src/app/services/application.service';
 import { NavigationService } from 'src/app/services/navigation.service';
 import { TitleService } from 'src/app/services/title.service';
+import { DeclarationComponent } from '../declaration/declaration.component';
 
 @Component({
-  selector: 'hse-declaration',
-  templateUrl: './declaration.component.html'
+  selector: 'hse-confirm',
+  templateUrl: './confirm.component.html'
 })
-export class DeclarationComponent extends BaseComponent implements OnInit {
-  static route: string = 'declaration';
-  static title: string = "Declaration - Register a high-rise building - GOV.UK";
+export class ConfirmComponent extends BaseComponent {
+  static route: string = 'information-submitted';
+  static title: string = "Structure and safety information submitted - Register a high-rise building - GOV.UK";
 
   constructor(router: Router, applicationService: ApplicationService, navigationService: NavigationService, activatedRoute: ActivatedRoute, titleService: TitleService) {
     super(router, applicationService, navigationService, activatedRoute, titleService);
   }
 
-  ngOnInit(): void {
-    this.applicationService.model.ApplicationStatus |= BuildingApplicationStatus.KbiSubmitInProgress;
-  }
-
   canContinue(): boolean {
-    this.applicationService.model.ApplicationStatus |= BuildingApplicationStatus.KbiSubmitComplete;
     return true;
   }
 
@@ -31,7 +27,7 @@ export class DeclarationComponent extends BaseComponent implements OnInit {
   }
 
   override canAccess(routeSnapshot: ActivatedRouteSnapshot) {
-    return this.containsFlag(BuildingApplicationStatus.KbiConnectionsComplete);
+    return this.containsFlag(BuildingApplicationStatus.KbiSubmitComplete);
   }
 
   containsFlag(flag: BuildingApplicationStatus) {
