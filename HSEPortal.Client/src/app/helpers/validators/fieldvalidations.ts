d14556440f94962b01970c372efe0591db9153c9
OBellochDiaz <odiaz@codec.ie>
2023-06-13 13:01:51 +0100

    Adding kbi field validations on check answers page.

--- HSEPortal.Client/src/app/helpers/validators/fieldvalidations.ts
+++ HSEPortal.Client/src/app/helpers/validators/fieldvalidations.ts
@@ -4,4 +4,6 @@ export class FieldValidations {
   static IsLessThanOrEqualTo100: (value: number | undefined) => boolean = (value) => value !== undefined && value <= 100;
   static IsAPositiveNumber: (value: number | undefined) => boolean = (value) => value !== undefined && value > -1;
   static IsWholeNumber: (value: number | undefined) => boolean = (value) => value !== undefined && (value % 1 === 0);
+  static IsNotNullOrEmpty: (value: any[] | undefined) => boolean = (value) => value !==undefined && value.length > 0;
+  static IsNotNullAndValuesAreNotEmpty: (value: Record<string, string[]> | undefined) => boolean = (value) => value !==undefined && Object.keys(value).length > 0 && Object.keys(value).every(x => !!value[x] && value[x].length > 0);
 }
