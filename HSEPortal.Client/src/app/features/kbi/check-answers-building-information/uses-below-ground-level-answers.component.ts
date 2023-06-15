f711f5603b1ae0ee2706b348de5962db419f3583
OBellochDiaz <odiaz@codec.ie>
2023-06-15 12:12:51 +0100

    Adding missing value

--- HSEPortal.Client/src/app/features/kbi/check-answers-building-information/uses-below-ground-level-answers.component.ts
+++ HSEPortal.Client/src/app/features/kbi/check-answers-building-information/uses-below-ground-level-answers.component.ts
@@ -27,6 +27,7 @@ export class UsesBelowGroundLevelAnswersComponent  extends BuildingInformationCh
   private buildingUseMapper: Record<string, string> = {
     "assembly_and_recreation": "Assembly and recreation",
     "assembly_recreation": "Assembly and recreation",
+    "residential_dwellings": "Residential dwellings",
     "office": "Office",
     "residential_institution": "Residential institution",
     "other_residential_use": "Other residential use",
