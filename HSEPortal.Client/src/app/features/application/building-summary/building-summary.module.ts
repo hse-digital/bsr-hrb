485c0a20f3998885038913b94cc7da646b6d9c04
OBellochDiaz <odiaz@codec.ie>
2023-07-26 16:16:43 +0100

    [WIP] Implementing screen #6498

--- HSEPortal.Client/src/app/features/application/building-summary/building-summary.module.ts
+++ HSEPortal.Client/src/app/features/application/building-summary/building-summary.module.ts
@@ -25,11 +25,13 @@ import { SectionYearOfCompletionComponent } from "./year-of-completion/year-of-c
 import { SectionYearRangeComponent } from "./year-range/year-range.component";
 import { NotNeedRegisterSingleStructureComponent } from './not-need-register-single-structure/not-need-register-single-structure.component';
 import { NotNeedRegisterMultiStructureComponent } from './not-need-register-multi-structure/not-need-register-multi-structure.component';
+import { NotNeedRegisterMultiDuplicatedStructuresComponent } from './duplicates/not-reg-multi-dupli-struct/not-register-multi-dupli-structures.component';
 
 const routes = new HseRoutes([
   HseRoute.protected(AddMoreSectionsComponent.route, AddMoreSectionsComponent, undefined),
   HseRoute.protected(MoreInformationComponent.route, MoreInformationComponent, MoreInformationComponent.title),
   HseRoute.protected(SectionCheckAnswersComponent.route, SectionCheckAnswersComponent, SectionCheckAnswersComponent.title),
+  HseRoute.protected(NotNeedRegisterMultiDuplicatedStructuresComponent.route, NotNeedRegisterMultiDuplicatedStructuresComponent, NotNeedRegisterMultiDuplicatedStructuresComponent.title),
   HseRoute.forChildren(':id', BuildingSummaryComponent, new HseRoutes([
     HseRoute.protected(SectionsIntroComponent.route, SectionsIntroComponent, SectionsIntroComponent.title),
     HseRoute.protected(SectionFloorsAboveComponent.route, SectionFloorsAboveComponent, SectionFloorsAboveComponent.title),
@@ -68,7 +70,8 @@ const routes = new HseRoutes([
     CertificateIssuerComponent,
     CertificateNumberComponent,
     NotNeedRegisterSingleStructureComponent,
-    NotNeedRegisterMultiStructureComponent
+    NotNeedRegisterMultiStructureComponent,
+    NotNeedRegisterMultiDuplicatedStructuresComponent
   ],
   imports: [
     RouterModule.forChild(routes.getRoutes()),
