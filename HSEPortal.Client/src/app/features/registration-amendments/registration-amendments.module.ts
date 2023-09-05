d7b6ad7af03dfee2e03d617321a5047f11dd314d
Orlando Belloch Diaz <ODiazcodecsystems@hsegov.onmicrosoft.com>
2023-09-05 08:22:46 +0000

    Merged PR 921: US9311 - registration amendments - Select what you want to do
    Implementing screen 9311
    Updating "home" and "select" navigation
    
    Related work items: #9311

---
+++ HSEPortal.Client/src/app/features/registration-amendments/registration-amendments.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HseAngularModule } from 'hse-angular';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { PipesModule } from 'src/app/pipes/pipes.module';
+import { HseRoutes } from 'src/app/services/hse.route';
+
+const routes = new HseRoutes([
+]);
+
+@NgModule({
+  declarations: [
+  ],
+  providers: [HttpClient, ...routes.getProviders()],
+  imports: [
+    RouterModule.forChild(routes.getRoutes()),
+    HseAngularModule,
+    CommonModule,
+    HttpClientModule,
+    ComponentsModule,
+    FormsModule,
+    PipesModule,
+  ]
+})
+export class RegistrationAmendmentsModule {
+  static baseRoute: string = "registration-amendments";
+}
---
+++ HSEPortal.Client/src/app/features/registration-amendments/what-want-to-do/what-want-to-do.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { PageComponent } from 'src/app/helpers/page.component';
+import { FieldValidations } from 'src/app/helpers/validators/fieldvalidations';
+import { ApplicationService } from 'src/app/services/application.service';
+import { KbiService } from 'src/app/services/kbi.service';
+
+@Component({
+  selector: 'hse-what-want-to-do',
+  templateUrl: './what-want-to-do.component.html'
+})
+export class WhatWantToDoComponent extends PageComponent<string> {
+  static route: string = 'what-you-want-to-do';
+  static title: string = "Select what you want to do - Register a high-rise building - GOV.UK";
+
+  constructor(activatedRoute: ActivatedRoute, private kbiService: KbiService) {
+    super(activatedRoute);
+  }
+
+  override async onInit(applicationService: ApplicationService): Promise<void> {
+    this.updateOnSave = false;
+  }
+
+  override async onSave(applicationService: ApplicationService): Promise<void> {
+    
+  }
+
+  override canAccess(applicationService: ApplicationService, routeSnapshot: ActivatedRouteSnapshot): boolean {
+    return true;
+  }
+
+  override isValid(): boolean {
+    return FieldValidations.IsNotNullOrWhitespace(this.model);
+  }
+
+  override async navigateNext(): Promise<boolean | void> {
+    if(this.model == 'register-building') {
+      return this.navigationService.navigateRelative('/select', this.activatedRoute);
+    }
+    return true;
+  }
+
+}
