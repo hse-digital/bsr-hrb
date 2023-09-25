bbe3c1025b8ea2264997607af25e8f7b5967c582
OBellochDiaz <odiaz@codec.ie>
2023-09-25 10:51:27 +0100

    Added isStructureAddress condition to address component.

--- HSEPortal.Client/src/app/components/address/address.component.ts
+++ HSEPortal.Client/src/app/components/address/address.component.ts
@@ -13,6 +13,7 @@ export class AddressComponent implements OnInit {
   @Input() address?: AddressModel;
   @Input() addressName!: string;
   @Input() selfAddress = false;
+  @Input() isStructureAddress: boolean = false;
   @Output() onAddressConfirmed = new EventEmitter();
   @Output() onChangeStep = new EventEmitter();
 
