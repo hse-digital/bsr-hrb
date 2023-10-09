3611d1f459c4e14fa88561d6550bb7fb7a8a5a87
OBellochDiaz <odiaz@codec.ie>
2023-10-09 17:08:40 +0100

    Added "upload files" functionality in the API

---
+++ HSEPortal.API/BlobStore/BlobSASUri.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using HSE.BsrHelp.API.Services;
+using Microsoft.Extensions.Options;
+
+namespace HSEPortal.API.BlobStore;
+
+public interface IBlobSASUri 
+{
+    string GetSASUri(string blobName);
+}
+public class BlobSASUri : IBlobSASUri
+{
+    private readonly IBlobClient blobClient;
+    private readonly BlobStoreOptions blobStoreOptions;
+
+    public BlobSASUri(IBlobClient blobClient, IOptions<BlobStoreOptions> blobStoreOptions)
+    {
+        this.blobClient = blobClient;
+        this.blobStoreOptions = blobStoreOptions.Value;
+    }
+
+    public string GetSASUri(string blobName)
+    {        
+        var blobServiceClient = new BlobServiceClient(blobStoreOptions.ConnectionString);
+
+        //  Gets a reference to the container.
+        var containerClient = blobServiceClient.GetBlobContainerClient(blobStoreOptions.ContainerName);
+
+        //  Gets a reference to the blob in the container
+        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+        Uri fullUri = blobClient.GenerateSasUri(BlobSasPermissions.All, DateTime.UtcNow.AddDays(1));
+
+        return fullUri.ToString();
+    }
+}
---
+++ HSEPortal.API/BlobStore/BlobStoreClient.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Blobs;
+using HSE.BsrHelp.API.Services;
+using Microsoft.Extensions.Options;
+
+namespace HSEPortal.API.BlobStore;
+
+public interface IBlobClient 
+{
+    BlobServiceClient GetBlobServiceClient();
+}
+public class BlobStoreClient : IBlobClient
+{
+    private readonly BlobStoreOptions blobStoreOptions;
+
+    public BlobStoreClient(IOptions<BlobStoreOptions> blobStoreOptions) 
+    {
+        this.blobStoreOptions = blobStoreOptions.Value;
+    }
+    public BlobServiceClient GetBlobServiceClient()
+    {
+        return new BlobServiceClient(blobStoreOptions.ConnectionString);
+
+    } 
+}
