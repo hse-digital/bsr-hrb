f979188994d6cee6009dfe16752058eeca2a5f9d
OBellochDiaz <odiaz@codec.ie>
2023-10-23 12:45:46 +0100

    Updated ChangeBuildingSummary status variable

--- HSEPortal.Client/src/app/features/registration-amendments/change-building-summary/building-change-check-answers/building-change-check-answers.component.ts
+++ HSEPortal.Client/src/app/features/registration-amendments/change-building-summary/building-change-check-answers/building-change-check-answers.component.ts
@@ -12,6 +12,7 @@ import { FieldValidations } from 'src/app/helpers/validators/fieldvalidations';
 import { AddressModel } from 'src/app/services/address.service';
 import { ApplicationService, BuildingApplicationStage, ChangeBuildingSummary, ChangeSection, OutOfScopeReason, SectionModel, Status } from 'src/app/services/application.service';
 import { RemoveStructureComponent } from '../remove-structure/remove-structure.component';
+import { ChangeTaskListComponent } from '../../change-task-list/change-task-list.component';
 
 @Component({
   selector: 'hse-building-change-check-answers',
@@ -73,6 +74,17 @@ export class BuildingChangeCheckAnswersComponent  extends PageComponent<void> {
     this.initStatecode();
     this.initChangeBuildingSummary();
     this.activeSections = this.getSections();
+    this.updateBuildingChangeStatus();
+  }
+
+  private updateBuildingChangeStatus() {
+    if(this.hasBuildingChange()) {
+      this.applicationService.model.RegistrationAmendmentsModel!.ChangeBuildingSummary!.Status = this.validateModel() 
+        ? Status.ChangesComplete 
+        : Status.ChangesInProgress;
+    } else {
+      this.applicationService.model.RegistrationAmendmentsModel!.ChangeBuildingSummary!.Status = Status.NoChanges;
+    }
   }
 
   override canAccess(applicationService: ApplicationService, routeSnapshot: ActivatedRouteSnapshot): boolean {
@@ -80,7 +92,13 @@ export class BuildingChangeCheckAnswersComponent  extends PageComponent<void> {
   }
 
   override isValid(): boolean {
-    var canContinue = true;
+    var canContinue = this.validateModel();
+    this.hasIncompleteData = !canContinue;
+    return canContinue;
+  }
+
+  private validateModel() {
+    let canContinue = true;
     for (var section of this.activeSections) {
       if (this.applicationService.model.NumberOfSections == "two_or_more") {
         canContinue &&= FieldValidations.IsNotNullOrWhitespace(section.Name);
@@ -105,8 +123,6 @@ export class BuildingChangeCheckAnswersComponent  extends PageComponent<void> {
         }
       }
     }
-
-    this.hasIncompleteData = !canContinue;
     return canContinue;
   }
 
@@ -120,7 +136,7 @@ export class BuildingChangeCheckAnswersComponent  extends PageComponent<void> {
       return this.navigationService.navigateRelative(MoreInformationComponent.route, this.activatedRoute);
     }
 
-    return this.navigationService.navigateRelative(`../${AccountablePersonModule.baseRoute}/${AccountablePersonComponent.route}`, this.activatedRoute);
+    return this.navigationService.navigateRelative(ChangeTaskListComponent.route, this.activatedRoute);
   }
 
   override async onSave(): Promise<void> {
@@ -181,8 +197,44 @@ export class BuildingChangeCheckAnswersComponent  extends PageComponent<void> {
   }
 
   getLatestValueOf(field?: any, changedField?: any) {
-    let hasChanged = typeof changedField == "string" ? FieldValidations.IsNotNullOrWhitespace(changedField) : changedField != undefined;
+    let hasChanged = this.hasChanged(field, changedField);
     return hasChanged ? changedField : field;
+  }
+
+  hasBuildingChange() {
+    return this.applicationService.model.Sections.some((section, index) => {
+      let changedSections = this.applicationService.model.RegistrationAmendmentsModel?.ChangeBuildingSummary?.Sections ?? new Array<ChangeSection>(this.applicationService.model.Sections.length);
+      return this.hasSectionChange(section, changedSections[index]?.SectionModel ?? new SectionModel());
+    });
+  }
+ 
+  hasSectionChange(section: SectionModel, changedSection: SectionModel): boolean {
+    let hasChanged = false;
+    hasChanged ||= this.hasChanged(section.Name, changedSection.Name);
+    hasChanged ||= this.hasChanged(section.FloorsAbove, changedSection.FloorsAbove);
+    hasChanged ||= this.hasChanged(section.Height, changedSection.Height);
+    hasChanged ||= this.hasChanged(section.ResidentialUnits, changedSection.ResidentialUnits);
+    hasChanged ||= this.hasChanged(section.WhoIssuedCertificate, changedSection.WhoIssuedCertificate);
+    hasChanged ||= this.hasChanged(section.YearOfCompletion, changedSection.YearOfCompletion);
+    hasChanged ||= this.hasChanged(section.YearOfCompletionOption, changedSection.YearOfCompletionOption);
+    hasChanged ||= this.hasChanged(section.YearOfCompletionRange, changedSection.YearOfCompletionRange);
+    hasChanged ||= this.hasChanged(section.CompletionCertificateIssuer, changedSection.CompletionCertificateIssuer);
+    hasChanged ||= this.hasChanged(section.CompletionCertificateReference, changedSection.CompletionCertificateReference);
+    hasChanged ||= this.hasChanged(section.CompletionCertificateFile, changedSection.CompletionCertificateFile);
+    hasChanged ||= this.hasChanged(section.CompletionCertificateDate, changedSection.CompletionCertificateDate);
+    hasChanged ||= this.hasAddressChanged(section.Addresses, changedSection.Addresses);
+    return hasChanged; 
+  }
+  
+  hasChanged(field?: any, changedField?: any) {
+    let hasNewValue = typeof changedField == "string" ? FieldValidations.IsNotNullOrWhitespace(changedField) : changedField != undefined;
+    return hasNewValue && field != changedField;
+  }
+
+  hasAddressChanged(SectionAddresses: AddressModel[], ChangeSectionAddresses: AddressModel[]) {
+    return SectionAddresses.some((element, index) => {
+      return this.hasChanged(element, ChangeSectionAddresses[index]);
+    }); 
   }
 
 }
