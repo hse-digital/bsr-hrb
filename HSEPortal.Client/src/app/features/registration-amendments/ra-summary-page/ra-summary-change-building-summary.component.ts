735c8f632c83bf5a909bbe0f9d89262e2b4226cb
OBellochDiaz <odiaz@codec.ie>
2023-12-19 10:52:51 +0000

    Updated building, kbi and connections

--- HSEPortal.Client/src/app/features/registration-amendments/ra-summary-page/ra-summary-change-building-summary.component.ts
+++ HSEPortal.Client/src/app/features/registration-amendments/ra-summary-page/ra-summary-change-building-summary.component.ts
@@ -16,4 +16,31 @@ export class RaSummaryChangeBuildingSummaryComponent {
         return value.split(',');
     }
 
+    private summaryFields = [
+        "Name",
+        "number of floors",
+        "height",
+        "number of residential units",
+        "who issued certificate",
+        "completion certificate issuer",
+        "completion certificate reference",
+        "completion certificate file",
+        "addresses",
+        "built?"
+    ];
+
+    private isSummaryChange = (fieldname?: string) => { return this.summaryFields.some(field => fieldname?.trim().endsWith(field))};
+    
+    get summaryChanges() {
+        return this.changes.filter(x => this.isSummaryChange(x.FieldName));
+    }
+
+    get connectionChanges() {
+        return this.changes.filter(x => x.FieldName?.startsWith("Conn"));
+    }
+
+    get kbiChanges() {
+        return this.changes.filter(x => !this.isSummaryChange(x.FieldName) && !x.FieldName?.startsWith("Conn"));
+    }
+
 }
