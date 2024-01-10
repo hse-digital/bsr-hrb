309bb6d9ee6990e9939b9db2f7c26e56c7fd3c24
Diego Santin <Diego Santin>
2024-01-10 13:06:41 -0300

    structure details

--- HSEPortal.API/Functions/PublicRegisterFunctions.cs
+++ HSEPortal.API/Functions/PublicRegisterFunctions.cs
@@ -9,15 +9,24 @@ public class PublicRegisterFunctions
 {
     [Function(nameof(SearchPublicRegister))]
     public Task<HttpResponseData> SearchPublicRegister([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData request,
-        [CosmosDBInput("hseportal", "building-registrations", SqlQuery = "SELECT c.id, c.ApplicationStatus, c.BuildingName, c.CurrentVersion.Sections as Sections FROM c JOIN s IN c.CurrentVersion.Sections JOIN a IN s.Addresses WHERE a.Postcode = {postcode} OR a.PostcodeEntered = {postcode}",
+        [CosmosDBInput("hseportal", "building-registrations", SqlQuery = "SELECT c.id, c.ContactFirstName, c.ContactLastName, c.ApplicationStatus, c.BuildingName, c.CurrentVersion.Sections as Sections, c.CurrentVersion.AccountablePersons as AccountablePersons FROM c JOIN s IN c.CurrentVersion.Sections JOIN a IN s.Addresses WHERE a.Postcode = {postcode} OR a.PostcodeEntered = {postcode}",
             Connection = "CosmosConnection")]
         List<PublicRegisterApplicationModel> buildingApplications,
-        [CosmosDBInput("hseportal", "building-registrations", SqlQuery = "SELECT c.id, c.ApplicationStatus, c.BuildingName, c.Sections FROM c JOIN s IN c.Sections JOIN a IN s.Addresses WHERE a.Postcode = {postcode} OR a.PostcodeEntered = {postcode}",
+        [CosmosDBInput("hseportal", "building-registrations", SqlQuery = "SELECT c.id, c.ContactFirstName, c.ContactLastName, c.ApplicationStatus, c.BuildingName, c.Sections, c.AccountablePersons as AccountablePersons FROM c JOIN s IN c.Sections JOIN a IN s.Addresses WHERE a.Postcode = {postcode} OR a.PostcodeEntered = {postcode}",
             Connection = "CosmosConnection")]
         List<PublicRegisterApplicationModel> nonVersionedApplications)
     {
         var registeredApplications = buildingApplications.Concat(nonVersionedApplications)
-            .DistinctBy(x => x.id).Where(x => x.ApplicationStatus.HasFlag(BuildingApplicationStatus.PaymentComplete)).ToList();
+            .DistinctBy(x => x.id).Where(x => x.ApplicationStatus.HasFlag(BuildingApplicationStatus.PaymentComplete))
+            .Select(x => x.Sections.Select(section => new PublicRegisterStructureModel
+            {
+                ApplicationId = x.id,
+                ContactLastName = x.ContactLastName,
+                ContactFirstName = x.ContactFirstName,
+                BuildingName = x.BuildingName,
+                Structure = section,
+                AccountablePersons = x.AccountablePersons
+            })).SelectMany(x => x).ToList();
         
         return request.CreateObjectResponseAsync(registeredApplications);
     }
@@ -26,7 +35,20 @@ public class PublicRegisterFunctions
 public class PublicRegisterApplicationModel
 {
     public string id { get; set; }   
+    public string ContactFirstName { get; set; }
+    public string ContactLastName { get; set; }
     public BuildingApplicationStatus ApplicationStatus { get; set; }
     public string BuildingName { get; set; }
     public List<SectionModel> Sections { get; set; }
+    public List<AccountablePerson> AccountablePersons { get; set; }
+}
+
+public class PublicRegisterStructureModel
+{
+    public string ApplicationId { get; set; }
+    public string ContactFirstName { get; set; }
+    public string ContactLastName { get; set; }
+    public string BuildingName { get; set; }
+    public SectionModel Structure { get; set; }
+    public List<AccountablePerson> AccountablePersons { get; set; }
 }
