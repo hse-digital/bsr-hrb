6a29d1593d28f720b48881e50683b1732ab261a9
Diego Santin <dsantin@codec.ie>
2024-02-07 11:10:26 +0000

    Merged PR 1933: disabling password protection
    Related work items: #20470, #21050, #21073, #21078, #21295, #21300, #21305, #21321, #21392

--- HSEPortal.Client/src/app/features/public-register/public-register.module.ts
+++ HSEPortal.Client/src/app/features/public-register/public-register.module.ts
@@ -1,35 +1,22 @@
-import { NgModule, inject } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { SearchPublicRegisterComponent } from "./search-register/search-register.component";
 import { StructureNotFoundComponent } from "./structure-not-found/structure-not-found.component";
 import { HttpClientModule } from "@angular/common/http";
 import { CommonModule } from "@angular/common";
 import { HseAngularModule } from "hse-angular";
 import { ComponentsModule } from "src/app/components/components.module";
-import { ActivatedRouteSnapshot, Route, Router, RouterModule, RouterStateSnapshot } from "@angular/router";
+import { Route, RouterModule } from "@angular/router";
 import { PublicRegisterResultsComponent } from "./results/results.component";
 import { StructureDetailsComponent } from "./structure-details/structure-details.component";
 import { PipesModule } from "../../pipes/pipes.module";
 import { PublicRegisterService } from "src/app/services/public-register.service";
 import { PasswordAccessComponent } from "./password-access/password-access.component";
 
-const publicRegisterGuard = async (_: ActivatedRouteSnapshot, __: RouterStateSnapshot) => {
-    var service = inject(PublicRegisterService);
-    var router = inject(Router);
-
-    var canAccess = await service.canAccessPublicRegister();
-    if (!canAccess) {
-        router.navigate(['/public-register/password']);
-    }
-
-    return canAccess;
-};
-
 const angRoutes: Route[] = [
-    { path: SearchPublicRegisterComponent.route, component: SearchPublicRegisterComponent, canActivate: [publicRegisterGuard], title: SearchPublicRegisterComponent.title},
-    { path: StructureNotFoundComponent.route, component: StructureNotFoundComponent, canActivate: [publicRegisterGuard], title: StructureNotFoundComponent.title},
-    { path: PublicRegisterResultsComponent.route, component: PublicRegisterResultsComponent, canActivate: [publicRegisterGuard], title: PublicRegisterResultsComponent.title},
-    { path: StructureDetailsComponent.route, component: StructureDetailsComponent, canActivate: [publicRegisterGuard], title: StructureDetailsComponent.title},
-    { path: PasswordAccessComponent.route, component: PasswordAccessComponent },
+    { path: SearchPublicRegisterComponent.route, component: SearchPublicRegisterComponent},
+    { path: StructureNotFoundComponent.route, component: StructureNotFoundComponent},
+    { path: PublicRegisterResultsComponent.route, component: PublicRegisterResultsComponent},
+    { path: StructureDetailsComponent.route, component: StructureDetailsComponent}
 ];
 
 @NgModule({
